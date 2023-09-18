2df02156dc78fb03b1a79ce589a6a9e99703e676
Giacomo713 <giaccommoo@gmail.com>
2023-09-18 17:35:52 +0200

    test

--- test.js
+++ test.js
@@ -1,24 +1,16 @@
-console.log(4 + 5)
-console.log(2 + 6 +2 + 55 +11)
-console.log((4 + 6 + 9) / 77)
-
-let a = 10
-console.log(a)
-console.log(a * 9)
-
-let b = 7 * a
-console.log(b)
-
-const max = 57
-const actual = max - 13
-const percentage = actual / max
-
-const button = document.querySelector("button");
-
-function greet() {
-  const name = prompt("What is your name?");
-  const greeting = document.querySelector("#greeting");
-  greeting.textContent = `Hello ${name}, nice to see you!`;
+let username = prompt("whats your username", "")
+if (username === "Admin"){ 
+  let password = prompt("whats your password", "")
+  if (password === "Admin") {
+  alert("Welcome!")
+ }else if (password === "" || password === null){
+  alert("Cancelled!")
+ }else{
+  alert("wrong password")
+ } 
+ }else if (username === "" || username === null) {
+  alert("cancelled")
+ }else {
+  alert("wrong password")
 }
 
-button.addEventListener("click", greet);
